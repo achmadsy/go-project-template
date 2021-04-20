bd56da51a36a3a2baa5f2c990b64d62c5c138968
arif <achmadsy@live.com>
2021-04-20 09:34:49 +0700

    Correct names and paths

--- go-project/go.mod
+++ go-project/go.mod
@@ -1,3 +1,3 @@
-module github.com/achmadsy/go-project
+module github.com/achmadsy/go-project-template/go-project
 
 go 1.16
--- go-project/main.go
+++ go-project/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/achmadsy/go-project-template/go-project/db"
+	"github.com/achmadsy/go-project-template/go-project/configs/db"
 	"github.com/achmadsy/go-project-template/go-project/routes"
 )
 
